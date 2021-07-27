94755288106254854043a14cc5222fd369ed2844
José João Silva <josejoaosilva.n@gmail.com>
2021-07-27 18:28:12 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package (
     targets: [
         .target(
             name: "HJVisualComponents",
-            path: "HJVisualComponents"
+            path: "HJVisualComponents")
     ]
 )
