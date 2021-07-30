c55a6ccd43c2607b8459a1b5f400a99ad9feaa9e
Joao Silva <josejoaosilva.n@gmail.com>
2021-07-30 10:48:50 -0300

    create oritantione enum

--- HJVisualComponents/RadioButton.h
+++ HJVisualComponents/RadioButton.h
@@ -7,12 +7,19 @@
 
 #import <UIKit/UIKit.h>
 IB_DESIGNABLE
+
+typedef NS_ENUM(NSInteger, Orientation) {
+  vertical,
+  horizontal
+};
+
 @interface RadioButton: UIView{
     CGFloat multiplier;
     UIColor *selectedColor, *unselectedColor, *fontColor;
     NSArray *names;
     NSArray *buttons;
     long selectedButton;
+    Orientation orientation;
 }
 @property (readonly) NSArray *buttons;
 @property (readonly) long selectedButton;
@@ -29,13 +36,17 @@ IB_DESIGNABLE
 /// @brief Button's size relative to parent view.
 @property (nonatomic) IBInspectable CGFloat multiplier;
 
+/// @brief Button's orientation. Can ben horizontgal or vertical.
+@property (nonatomic) IBInspectable Orientation orientation;
+
 /// @brief Configure the radio buttons with custom design.
 /// @param multiplierP CGFloat that represents button size relative to parent view.
 /// @param selectedColorP UIColor * that represents button color when selected.
 /// @param unselectedColorP UIColor * that represents button color when not selected
 /// @param fontColorP UIColor * that represents color of the label below the button
+/// @param orientationP Orientation that represents the button's orientation. Can ben horizontgal or vertical.
 /// @returns RadioButton
--(instancetype)initWithMultiplier:(CGFloat)multiplierP selectedColor:(UIColor*)selectedColorP unselectedColor:(UIColor*)unselectedColorP fontColor:(UIColor*)fontColorP;
+-(instancetype)initWithMultiplier:(CGFloat)multiplierP selectedColor:(UIColor*)selectedColorP unselectedColor:(UIColor*)unselectedColorP fontColor:(UIColor*)fontColorP orientation:(Orientation)orientationP;
 
 /// @brief Configure the quantity and values of radio button's options. The options must be a String Array.
 /// @discussion Note that the namesP variable must be a type that can be converted to NSString.
--- HJVisualComponents/RadioButton.m
+++ HJVisualComponents/RadioButton.m
@@ -9,13 +9,13 @@
 
 @implementation RadioButton: UIView
 
-
-- (id)initWithMultiplier:(CGFloat)multiplierP selectedColor:(UIColor *)selectedColorP unselectedColor:(UIColor *)unselectedColorP fontColor:(UIColor *)fontColorP {
+- (id)initWithMultiplier:(CGFloat)multiplierP selectedColor:(UIColor *)selectedColorP unselectedColor:(UIColor *)unselectedColorP fontColor:(UIColor *)fontColorP orientation:(Orientation)orientationP {
     if ((self = [super init])) {
         multiplier = multiplierP;
         selectedColor = selectedColorP;
         unselectedColor = unselectedColorP;
         fontColor = fontColorP;
+        orientation = orientationP;
     }
     
     return self;
@@ -49,6 +49,7 @@
     fontColor = [UIColor blackColor];
     buttons = [[NSArray alloc] init];
     selectedButton = -1;
+    orientation = horizontal;
 }
 
 -(void)wasTapped:(UIButton*)sender {
@@ -72,15 +73,26 @@
 }
 
 -(void)setUpRadioButtons {
-    CGFloat columnSize = self.frame.size.width / names.count;
-    CGFloat step = columnSize/2;
+    CGFloat columnSize;
+    CGFloat step;
+    
+    switch (orientation) {
+        case horizontal:
+            columnSize = self.frame.size.width / names.count;
+            step = columnSize/2;
+            break;
+        case vertical:
+            columnSize = self.frame.size.height / names.count;
+            step = columnSize/2;
+            break;
+    }
     
     for (int i = 0; i < names.count; i++) {
         UIButton *newButton = [self setUpButton];
         [newButton setTag:i+1];
         UILabel *newLabel = [self setUpLabel:names[i]];
         
-        [self setUPConstraints:newButton withLabel:newLabel withXPosition:step+(columnSize*i)];
+        [self setUPConstraints:newButton withLabel:newLabel withPosition:step+(columnSize*i)];
     }
 }
 
@@ -114,20 +126,37 @@
     return newLabel;
 }
 
--(void) setUPConstraints:(UIButton*)newButton withLabel:(UILabel*)newLabel withXPosition:(CGFloat)xPosition {
+-(void) setUPConstraints:(UIButton*)newButton withLabel:(UILabel*)newLabel withPosition:(CGFloat)position {
     [self addSubview:newButton];
     [self addSubview:newLabel];
-
-    newButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [newButton.centerXAnchor constraintEqualToAnchor:self.leftAnchor constant:xPosition].active = YES;
-    [newButton.topAnchor constraintEqualToAnchor:self.topAnchor].active = YES;
-    [newButton.heightAnchor constraintEqualToAnchor:self.heightAnchor multiplier:multiplier].active = YES;
-    [newButton.widthAnchor constraintEqualToAnchor:self.heightAnchor multiplier:multiplier].active = YES;
     
+    newButton.translatesAutoresizingMaskIntoConstraints = NO;
     newLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    [newLabel.topAnchor constraintEqualToAnchor:newButton.bottomAnchor constant:5].active = YES;
-    [newLabel.centerXAnchor constraintEqualToAnchor:newButton.centerXAnchor].active = YES;
+    
     [newLabel.widthAnchor constraintLessThanOrEqualToConstant: (self.frame.size.width/names.count)-5].active = YES;
+    
+    switch (orientation) {
+        case horizontal:
+            [newButton.centerXAnchor constraintEqualToAnchor:self.leftAnchor constant:position].active = YES;
+            [newButton.topAnchor constraintEqualToAnchor:self.topAnchor].active = YES;
+            [newButton.heightAnchor constraintEqualToAnchor:self.heightAnchor multiplier:multiplier].active = YES;
+            [newButton.widthAnchor constraintEqualToAnchor:self.heightAnchor multiplier:multiplier].active = YES;
+            
+            [newLabel.topAnchor constraintEqualToAnchor:newButton.bottomAnchor constant:5].active = YES;
+            [newLabel.centerXAnchor constraintEqualToAnchor:newButton.centerXAnchor].active = YES;
+            break;
+            
+        case vertical:
+            [newButton.centerYAnchor constraintEqualToAnchor:self.topAnchor constant:position].active = YES;
+            [newButton.leftAnchor constraintEqualToAnchor:self.leftAnchor].active = YES;
+            [newButton.heightAnchor constraintEqualToAnchor:self.widthAnchor multiplier:multiplier].active = YES;
+            [newButton.widthAnchor constraintEqualToAnchor:self.widthAnchor multiplier:multiplier].active = YES;
+            
+            [newLabel.leftAnchor constraintEqualToAnchor:newButton.rightAnchor constant:5].active = YES;
+            [newLabel.centerYAnchor constraintEqualToAnchor:newButton.centerYAnchor].active = YES;
+            break;
+    }
 }
+
 
 @end
