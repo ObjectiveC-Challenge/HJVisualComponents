299c948a823826d25ff4d880458ba2e73f672ef7
Joao Silva <josejoaosilva.n@gmail.com>
2021-07-30 09:43:42 -0300

    add comments into projects

--- HJVisualComponents/RadioButton.m
+++ HJVisualComponents/RadioButton.m
@@ -9,12 +9,7 @@
 
 @implementation RadioButton: UIView
 
-/// @brief  Configure the radio buttons with custom design.
-/// @param multiplierP CGFloat that represents button size relative to parent view.
-/// @param selectedColorP UIColor * that represents button color when selected.
-/// @param unselectedColorP UIColor * that represents button color when not selected
-/// @param fontColorP UIColor * that represents color of the label below the button
-/// @returns RadioButton
+
 - (id)initWithMultiplier:(CGFloat)multiplierP selectedColor:(UIColor *)selectedColorP unselectedColor:(UIColor *)unselectedColorP fontColor:(UIColor *)fontColorP {
     if ((self = [super init])) {
         multiplier = multiplierP;
@@ -68,16 +63,10 @@
 }
 
 
-
-/// @brief Return the selected option. If there aren't select options, this function returns nil.
-/// @returns Return the type that was set in the funcion setOptions() or nil.
 -(id)getSelectedValue {
     return selectedButton < 0 ? nil : names[selectedButton];
 }
 
-/// @brief Configure the quantity and values of radio button's options. The options must be a String Array.
-/// @discussion Note that the namesP variable must be a type that can be converted to NSString.
-/// @param namesP NSArray .
 - (void)setOptions:(NSArray *)namesP {
     names = namesP;
 }
